712a4c0baf78406592e9ea020d3e1d3f47b71c3f
Stanley Masinde <stanmasinde@gmail.com>
2025-01-19 19:16:12 +0300

    ft: used tabs over spaces.
    I mean who in their right mind uses spaces?

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,9 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
+local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+o.expandtab = false -- Use actual tabs instead of spaces
+o.shiftwidth = 4 -- Number of spaces to use for each indentation level (optional, for consistency with tab width)
+o.tabstop = 4 -- Number of spaces a tab character is equal to
+o.softtabstop = 4 -- How many spaces are inserted when pressing `Tab` or `Backspace` in insert mode
