158efd97b3cd02e67bce7c0025ca0e3261cf7efe
Stanley Masinde <stanmasinde@gmail.com>
2025-02-01 15:19:22 +0300

    add Laravel plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -46,7 +46,7 @@ return {
     end,
   },
 
-  -- Rust support 
+  -- Rust support
   {
     "rust-lang/rust.vim",
     ft = "rust",
@@ -55,7 +55,26 @@ return {
     end,
   },
 
-  -- Debugger 
+  {
+    "adalessa/laravel.nvim",
+    dependencies = {
+      "tpope/vim-dotenv",
+      "nvim-telescope/telescope.nvim",
+      "MunifTanjim/nui.nvim",
+      "kevinhwang91/promise-async",
+    },
+    cmd = { "Laravel" },
+    keys = {
+      { "<leader>la", ":Laravel artisan<cr>" },
+      { "<leader>lr", ":Laravel routes<cr>" },
+      { "<leader>lm", ":Laravel related<cr>" },
+    },
+    event = { "VeryLazy" },
+    opts = {},
+    config = true,
+  },
+
+  -- Debugger
   {
     "mfussenegger/nvim-dap",
     config = function()
