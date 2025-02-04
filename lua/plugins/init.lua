b53504661ccf248d635701230528abcbdb13163f
Stanley Masinde <stanmasinde@gmail.com>
2025-02-04 16:47:59 +0300

    remove laravel plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -55,25 +55,6 @@ return {
     end,
   },
 
-  {
-    "adalessa/laravel.nvim",
-    dependencies = {
-      "tpope/vim-dotenv",
-      "nvim-telescope/telescope.nvim",
-      "MunifTanjim/nui.nvim",
-      "kevinhwang91/promise-async",
-    },
-    cmd = { "Laravel" },
-    keys = {
-      { "<leader>la", ":Laravel artisan<cr>" },
-      { "<leader>lr", ":Laravel routes<cr>" },
-      { "<leader>lm", ":Laravel related<cr>" },
-    },
-    event = { "VeryLazy" },
-    opts = {},
-    config = true,
-  },
-
   -- Debugger
   {
     "mfussenegger/nvim-dap",
