a47234f9bbfa64ce4b03e5bb15554ffb01230727
Stanley Masinde <stanmasinde@gmail.com>
2025-09-05 03:10:31 +0300

    chire: updates

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -317,6 +317,18 @@ return {
     end,
   },
 
+  {
+    "luckasRanarison/tailwind-tools.nvim",
+    name = "tailwind-tools",
+    build = ":UpdateRemotePlugins",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim", -- optional
+      "neovim/nvim-lspconfig",         -- optional
+    },
+    opts = {}                          -- your configuration
+  },
+
   -- DAP (Debug Adapter Protocol)
   {
     "mfussenegger/nvim-dap",
