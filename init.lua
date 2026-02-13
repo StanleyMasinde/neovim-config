603683ccde7dd93d0212e805028da2145a883e0e
Stanley Masinde <stanmasinde@gmail.com>
2026-02-13 18:29:01 +0300

    Change the diagnostics config the near default

--- init.lua
+++ init.lua
@@ -102,13 +102,7 @@ require("catppuccin").setup {
 vim.cmd.colorscheme "catppuccin"
 
 vim.diagnostic.config {
-  virtual_text = {
-    prefix = "●", -- small dot looks clean
-    spacing = 2,
-    source = "if_many", -- show source name if multiple
-  },
-  signs = true,
-  underline = true,
-  update_in_insert = false, -- don’t spam while typing
-  severity_sort = true,
+  virtual_text = true,
+  virtual_lines = true,
+  disabled = false,
 }
